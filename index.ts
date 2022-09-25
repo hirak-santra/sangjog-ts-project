f9408b4fb69c542b60557c04e0ab682b6b82ebe6
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2022-09-25 21:58:50 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-import { Sangjog } from "sangjog";
-import Application from "./src/Application";
+//Important setup need to be done first before importing app related files.
+import { Environment, Sangjog } from "sangjog";
+Environment.setup();
 
+
+//App started from here
+import Application from "./src/Application";
 Sangjog.register(Application);
 Sangjog.run();
