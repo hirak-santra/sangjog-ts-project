e33dc294fda9416a1507c11ede6fe8bafd4ac631
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2022-09-25 22:06:53 +0530

    Add files via upload

---
+++ src/app/@controllers/UserController.ts
@@ -0,0 +1,27 @@
+import UserService from "@services/UserService";
+import { Autowired, HttpHeader, HttpMethod, HttpRequest, HttpResponse, ApiMapping, ApiController } from "sangjog";
+
+@ApiController({
+    path: "/user",
+    version: 0
+})
+export default class UserController {
+
+    @Autowired 
+    userService: UserService;
+
+    //Get all list of owners.
+    @ApiMapping({ path: '/list', method: HttpMethod.GET, produces: HttpHeader.Json })
+    getUserList(req: HttpRequest, res: HttpResponse) {
+        res.setStatusCode(200);
+        res.sendResponse({body: "Hello World!"});
+        console.log('in getUserList method!!!!!!!!!!!!!!!!!!!!!!!');
+    }
+
+    //get an owner by owner id.
+    @ApiMapping({ path: 'list/:id', method: HttpMethod.GET })
+    getUser(req: HttpRequest, res: HttpResponse) {
+        console.log(req, res, 'getOwner')
+    }
+
+}
