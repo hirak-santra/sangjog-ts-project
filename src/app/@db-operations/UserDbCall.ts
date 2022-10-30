3af0dde954c2e253104a7f893232a4590cadd7a8
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2022-10-30 18:29:27 +0530

    Delete src/app/@db-operations directory

--- src/app/@db-operations/UserDbCall.ts
+++
@@ -1,9 +0,0 @@
-import DbConnection from "@db-connection/DbConnection";
-import { Autowired } from "sangjog";
-
-export default class UserDbCall {
-
-    @Autowired
-    connection: DbConnection;
-    
-}
