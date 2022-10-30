20339bb69e2f19db6c66f08bf0b6050b082e35e5
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2022-10-30 18:14:28 +0530

    Update Application.ts

--- src/Application.ts
+++ src/Application.ts
@@ -1,24 +1,32 @@
-import OwnerController from '@controllers/OwnerController';
-import UserController from '@controllers/UserController';
-import UserDbCall from '@db-operations/UserDbCall';
 import DbConnection from '@db-connection/DbConnection';
-import AppMiddlewire from '@http-interceptors/AppMiddlewire';
-import AuthService from '@services/AuthService';
-import UserService from '@services/UserService';
-import { AppConfiguration } from 'sangjog';
+import ClientModule from './app/ClientModule';
+import AdminModule from './app/AdminModule';
+import CommonAuthService from "@services/CommonAuthService";
+import { AppConfiguration } from 'sangjog/annotations';
 
 @AppConfiguration({
-    controllers: [UserController, OwnerController],
-    services: [UserService],
-    authentication: AuthService,
-    httpInterceptors: [AppMiddlewire],
-    dbCalls: [UserDbCall],
-    dbConnection: DbConnection
+    modules: [
+        {
+            id: "client",
+            apiDomainPath: '/api/client',
+            uiSourcePath: '/ui/client/WebContent',
+            uiServerPath: '/',
+            module: ClientModule
+        },
+        {
+            id: "admin",
+            apiDomainPath: '/api/admin',
+            isAdmin: true,
+            uiSourcePath: '/ui/admin/build',
+            uiServerPath: '/admin',
+            module: AdminModule
+        }
+    ],
+    auth: CommonAuthService,
+    dbConnection: [DbConnection]
 })
 export default class Application {
-
     public static main(arg: string[]) {
 
     }
-
 }
