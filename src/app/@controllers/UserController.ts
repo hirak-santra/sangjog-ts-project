b32b8b68e3b89406490395153c3f18afa1e8da84
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2022-11-12 10:26:10 +0530

    Update UserController.ts

--- src/app/@controllers/UserController.ts
+++ src/app/@controllers/UserController.ts
@@ -1,6 +1,6 @@
 import UserService from "@services/UserService";
 import { HttpHeader, HttpMethod, HttpRequest, HttpResponse } from "sangjog";
-import { ApiController, ApiMapping, Autowired } from "sangjog/annotations";
+import { ApiController, ApiMapping, Autowired, JsonData, JsonObject } from "sangjog/annotations";
 
 @ApiController({
     path: "/user",
@@ -11,12 +11,19 @@ export default class UserController {
     @Autowired 
     userService: UserService;
 
+    @JsonData('mockData') mockData: JsonObject;
+
     //Get all list of user.
     @ApiMapping({ path: '/list', method: HttpMethod.GET, produces: HttpHeader.Json })
     getUserList(req: HttpRequest, res: HttpResponse) {
-        res.setStatusCode(200);
-        res.sendResponse({body: "Hello World!"});
-        console.log('in getUserList method!!!!!!!!!!!!!!!!!!!!!!!');
+        try {
+            res.setStatusCode(200);
+            res.sendResponse({body: this.mockData});
+            console.log('in getUserList method!!!!!!!!!!!!!!!!!!!!!!!');
+        } catch(e) {
+            console.log(e)
+        }
+        
     }
 
 }
